9663561c10b705854aed35bda8bd5a07acb5e650
ilyass mar <ilyass.marghine22@gmail.com>
2025-10-08 16:37:32 +0200

    first commit

---
+++ src/main/java/com/clinicdigital/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.clinicdigital;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().write("<h1>Welcome to Clinic Digital!</h1><p>This is the home page .</p>");
+    }
+}
