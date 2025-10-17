35306d104c582e66452477af2c1a14bbe702b5bd
ilyass mar <ilyass.marghine22@gmail.com>
2025-10-17 16:55:58 +0200

    added the hashing library Bcrypt, and finished the user service

--- src/main/java/com/clinicdigital/service/UserService.java
+++ src/main/java/com/clinicdigital/service/UserService.java
@@ -3,6 +3,7 @@ package com.clinicdigital.service;
 import com.clinicdigital.model.User;
 import com.clinicdigital.repository.UserRepository;
 import com.clinicdigital.service.Interfaces.UserServiceInterface;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class UserService implements UserServiceInterface {
 
@@ -12,17 +13,36 @@ public class UserService implements UserServiceInterface {
         this.repo = new UserRepository();
     }
 
+    @Override
     public boolean register(User user){
         if (repo.findByEmail(user.getEmail()) != null){
             System.out.println("This email does already exist!");
             return false;
         }
-        String hashedPasswor = crypt(user.getPassword()); // Here we will create the hashing of the password
-        user.setPassword(hashedPasswor);
+        String hashedPassword =  hashPassword(user.getPassword());// Here we hash the password passed with the user to be registered
+        user.setPassword(hashedPassword);
         return repo.register(user);
     }
 
+    
+    private String hashPassword (String passwordToBeHashed){
+        return BCrypt.hashpw(passwordToBeHashed, BCrypt.gensalt(12));
+    }
+
+    @Override
     public User logIn(String email, String password){
-        return null;
+        User user = repo.findByEmail(email);
+        if(user != null){
+            if (BCrypt.checkpw(password, user.getPassword())){
+                return user ;
+            }else{
+                System.out.println("Sorry but the password is incorrect! ");
+                return null;
+            }
+        } else {
+            System.out.println("There is no user with such email");
+            return null;
+        }
+
     }
 }
